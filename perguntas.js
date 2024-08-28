98967ce7605f4e307337f0220397efeb91769f14
leticiaantoniassisouza <104445654+leticiaantoniassisouza@users.noreply.github.com>
2024-08-28 16:58:58 -0300

    perguntas

--- perguntas.js
+++ perguntas.js
@@ -1,19 +1,19 @@
 export const perguntas = [
     {
-        enunciado: " ",
+        enunciado: "Quem foi o principal criador de Elden Ring? ",
         alternativas: [
             {
-                texto: "",
+                texto: "Hidetaka Miyazaki",
                 afirmacao: [
-                    "",
+                    "Certo",
                     ""
                 ],
                 proxima: 1,
             },
             {
-                texto: "",
+                texto: "Hironobu Sakaguchi",
                 afirmacao: [
-                    "",
+                    "Errado",
                     ""
                 ],
                 proxima: 2,
@@ -21,21 +21,21 @@ export const perguntas = [
         ]
     },
     {
-        enunciado: "",
+        enunciado: "Qual é o nome do mundo onde Elden Ring se passa?",
         alternativas: [
             {
-                texto: "",
+                texto: "Godrick, o Enxertado",
                 afirmacao: [
-                    "",
+                    "Errado",
                     "",
                     ""
                 ],
                 proxima: 3,
             },
             {
-                texto: "",
+                texto: "Margit, o Agouro Caído",
                 afirmacao: [
-                    "",
+                    "Certo",
                     "",
                     ""
                 ],
