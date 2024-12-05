5316eca5e445597e4f7f4b3cd1aaae866c01899d
leticiaantoniassisouza <104445654+leticiaantoniassisouza@users.noreply.github.com>
2024-12-05 20:01:01 -0300

    ok

--- script.js
+++ script.js
@@ -9,6 +9,179 @@ const textoResultado = document.querySelector(".texto-resultado");
 const botaoJogarNovamente = document.querySelector(".novamente-btn");
 const botaoIniciar = document.querySelector(".iniciar-btn");
 const telaInicial = document.querySelector(".tela-inicial");
+const perguntas = [
+    {
+        enunciado: "Quem foi o principal criador de Elden Ring? ",
+        alternativas: [
+            {
+                texto: "Hidetaka Miyazaki",
+                afirmacao: [
+                    "Certo",
+                    ""
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Hironobu Sakaguchi",
+                afirmacao: [
+                    "Errado",
+                    ""
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Quem é o personagem principal do jogo?",
+        alternativas: [
+            {
+                texto: "O Cavaleiro",
+                afirmacao: [
+                    "Errado",
+                    "",
+                    ""
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "O Sinluz (Tarnished)",
+                afirmacao: [
+                    "Certo",
+                    "",
+                    ""
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual desses é um dos principais chefes em Elden Ring?",
+        alternativas: [
+            {
+                texto: "Gwyn, Lord of Cinder",
+                afirmacao: [
+                    "Errado",
+                    "",
+                    ""
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Malenia, Blade of Miquella",
+                afirmacao: [
+                    "Certo",
+                    "",
+                    ""
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é a natureza de Radahn, um dos grandes chefes do jogo?",
+        alternativas: [
+            {
+                texto: "Um guerreiro gigante",
+                afirmacao: [
+                    "Certo",
+                    ""
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Um cavaleiro celestial",
+                afirmacao: [
+                    "Errado",
+                    ""
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual renomado autor de fantasia colaborou com a FromSoftware para criar a lore de Elden Ring?",
+        alternativas: [
+            {
+                texto: "George R. R. Martin",
+                afirmacao: [
+                    "Certo",
+                    ""
+                ],
+                proxima: 5,
+            },
+            {
+                texto: " J.K. Rowling",
+                afirmacao: [
+                    "Errado",
+                    ""
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é a função das Sites of Grace no jogo?",
+        alternativas: [
+            {
+                texto: "Áreas seguras onde inimigos não podem atacar",
+                afirmacao: [
+                    "Errado",
+                    ""
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Pontos de recuperação de HP e FP, e lugares para subir de nível",
+                afirmacao: [
+                    "Certo",
+                    ""
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é o nome do mundo em que se passa Elden Ring?",
+        alternativas: [
+            {
+                texto: " Drangleic",
+                afirmacao: [
+                    "Errado",
+                    ""
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "The Lands Between",
+                afirmacao: [
+                    "Certo",
+                    ""
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual dos seguintes jogos da FromSoftware possui referências e semelhanças notáveis com Elden Ring?",
+        alternativas: [
+            {
+                texto: "Bloodborne",
+                afirmacao: [
+                    "Errado",
+                    ""
+                ],
+            },
+            {
+                texto: " Dark Souls",
+                afirmacao: [
+                    "Certo",
+                    "Temos um expert em Elden Ring por aqui!",
+                    "Parábens jovem rapaz gamer!"
+                ],
+            },
+        ]
+    }
+];
 
 let atual = 0;
 let perguntaAtual;
